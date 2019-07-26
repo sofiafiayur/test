baa0fd7d20254b726241a0d335a7bfe0873a5da2
sofiafiayur <48333612+sofiafiayur@users.noreply.github.com>
2019-07-26 18:35:07 +0200

    Add files via upload

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,57 @@
 import express from 'express';
+import mysql from 'mysql';
+// import env from './env.json';
+// tslint:disable-next-line: no-require-imports
+const mysqlModel = require('mysql-model');
+
 
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello World!'));
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
 
-console.log('hello yu');
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: '1234',
+    database: 'nsm',
+});
+
+connection.connect(function(err) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log('connected');
+    }
+});
+
+const appModel = mysqlModel.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: '1234',
+    database: 'nsm',
+});
+
+const node_tree = appModel.extend({
+    tabletableName: 'NODE_TREE',
+});
+
+const node_tree_names = appModel.extend({
+    tabletableName: 'NODE_TREE_NAMES',
+});
+
+let node_treeItem = new node_tree();
+let node_tree_name = new node_tree_names();
+
+console.log('node_treeItem', node_treeItem, 'node_tree_name', node_tree_name)
+
+connection.query('SELECT * FROM node_tree', (err, rows) => {
+    if (err) throw err;
+    console.log('Data received from Db:\n');
+    console.log(rows);
+  });
+
+
+
+// app.listen(3000, () => console.log('Example app listening on port 3000!'));
--- src/mysql/table.sql
+++ src/mysql/table.sql
@@ -10,50 +10,51 @@ CREATE TABLE node_tree (
     KEY (idNode)
 );
 
-INSERT INTO node_tree VALUES (1,2,2,3);
-INSERT INTO node_tree VALUES (2,2,4,5);
-INSERT INTO node_tree VALUES (3,2,6,7);
-INSERT INTO node_tree VALUES (4,2,8,9);
-INSERT INTO node_tree VALUES (5,1,1,24);
-INSERT INTO node_tree VALUES (6,2,10,11);
-INSERT INTO node_tree VALUES (7,2,12,19);
-INSERT INTO node_tree VALUES (8,3,15,16);
-INSERT INTO node_tree VALUES (9,3,17,18);
-INSERT INTO node_tree VALUES (10,2,20,21);
-INSERT INTO node_tree VALUES (11,3,13,14);
-INSERT INTO node_tree VALUES (12,2,22,23);
+INSERT INTO node_tree (level, iLeft, iRight) VALUES (2,2,3);
+INSERT INTO node_tree (level, iLeft, iRight) VALUES (2,4,5);
+INSERT INTO node_tree (level, iLeft, iRight) VALUES (2,6,7);
+INSERT INTO node_tree (level, iLeft, iRight) VALUES (2,8,9);
+INSERT INTO node_tree (level, iLeft, iRight) VALUES (1,1,24);
+INSERT INTO node_tree (level, iLeft, iRight) VALUES (2,10,11);
+INSERT INTO node_tree (level, iLeft, iRight) VALUES (2,12,19);
+INSERT INTO node_tree (level, iLeft, iRight) VALUES (3,15,16);
+INSERT INTO node_tree (level, iLeft, iRight) VALUES (3,17,18);
+INSERT INTO node_tree (level, iLeft, iRight) VALUES (12,20,21);
+INSERT INTO node_tree (level, iLeft, iRight) VALUES (13,13,14);
+INSERT INTO node_tree (level, iLeft, iRight) VALUES (12,22,23);
 
 
 CREATE TABLE node_tree_names (
-    idNode int8 NOT NULL AUTO_INCREMENT,
+	idName int8 NOT NULL AUTO_INCREMENT,
+    idNode int8 NOT NULL,
     language varchar(50) NOT NULL,
     nodeName varchar(50) NOT NULL,
-    PRIMARY KEY(idNode),
-    KEY (idNode)
+    PRIMARY KEY(idName),
+    KEY (idName)
 );
 
-INSERT INTO node_tree_names VALUE (1, 'english', 'Marketing');
-INSERT INTO node_tree_names VALUE (2, 'english', 'Helpdesk');
-INSERT INTO node_tree_names VALUE (3, 'english', 'Managers');
-INSERT INTO node_tree_names VALUE (4, 'english', 'Customer Account');
-INSERT INTO node_tree_names VALUE (5, 'english', 'Docebo');
-INSERT INTO node_tree_names VALUE (6, 'english', 'Accounting');
-INSERT INTO node_tree_names VALUE (7, 'english', 'Sales');
-INSERT INTO node_tree_names VALUE (8, 'english', 'Italy');
-INSERT INTO node_tree_names VALUE (9, 'english', 'Europe');
-INSERT INTO node_tree_names VALUE (10, 'english', 'Developers');
-INSERT INTO node_tree_names VALUE (11, 'english', 'North America');
-INSERT INTO node_tree_names VALUE (12, 'english', 'Quality Assurance');
-INSERT INTO node_tree_names VALUE (1, 'italian', 'Marketing');
-INSERT INTO node_tree_names VALUE (2, 'italian', 'Supporto tecnico');
-INSERT INTO node_tree_names VALUE (3, 'italian', 'Managers');
-INSERT INTO node_tree_names VALUE (4, 'italian', 'Assistenza Cliente');
-INSERT INTO node_tree_names VALUE (5, 'italian', 'Docebo');
-INSERT INTO node_tree_names VALUE (6, 'italian', 'Amministrazione');
-INSERT INTO node_tree_names VALUE (7, 'italian', 'Supporto Vendite');
-INSERT INTO node_tree_names VALUE (8, 'italian', 'Italia');
-INSERT INTO node_tree_names VALUE (9, 'italian', 'Europa');
-INSERT INTO node_tree_names VALUE (10, 'italian', 'Sviluppatori');
-INSERT INTO node_tree_names VALUE (11, 'italian', 'Nord America');
-INSERT INTO node_tree_names VALUE (12, 'italian', 'Controllo Qualità');
+INSERT INTO node_tree_names (idNode, language, nodeName) VALUE (1, 'english', 'Marketing');
+INSERT INTO node_tree_names (idNode, language, nodeName) VALUE (2, 'english', 'Helpdesk');
+INSERT INTO node_tree_names (idNode, language, nodeName) VALUE (3, 'english', 'Managers');
+INSERT INTO node_tree_names (idNode, language, nodeName) VALUE (4, 'english', 'Customer Account');
+INSERT INTO node_tree_names (idNode, language, nodeName) VALUE (5, 'english', 'Docebo');
+INSERT INTO node_tree_names (idNode, language, nodeName) VALUE (6, 'english', 'Accounting');
+INSERT INTO node_tree_names (idNode, language, nodeName) VALUE (7, 'english', 'Sales');
+INSERT INTO node_tree_names (idNode, language, nodeName) VALUE (8, 'english', 'Italy');
+INSERT INTO node_tree_names (idNode, language, nodeName) VALUE (9, 'english', 'Europe');
+INSERT INTO node_tree_names (idNode, language, nodeName) VALUE (10, 'english', 'Developers');
+INSERT INTO node_tree_names (idNode, language, nodeName) VALUE (11, 'english', 'North America');
+INSERT INTO node_tree_names (idNode, language, nodeName) VALUE (12, 'english', 'Quality Assurance');
+INSERT INTO node_tree_names (idNode, language, nodeName) VALUE (1, 'italian', 'Marketing');
+INSERT INTO node_tree_names (idNode, language, nodeName) VALUE (2, 'italian', 'Supporto tecnico');
+INSERT INTO node_tree_names (idNode, language, nodeName) VALUE (3, 'italian', 'Managers');
+INSERT INTO node_tree_names (idNode, language, nodeName) VALUE (4, 'italian', 'Assistenza Cliente');
+INSERT INTO node_tree_names (idNode, language, nodeName) VALUE (5, 'italian', 'Docebo');
+INSERT INTO node_tree_names (idNode, language, nodeName) VALUE (6, 'italian', 'Amministrazione');
+INSERT INTO node_tree_names (idNode, language, nodeName) VALUE (7, 'italian', 'Supporto Vendite');
+INSERT INTO node_tree_names (idNode, language, nodeName) VALUE (8, 'italian', 'Italia');
+INSERT INTO node_tree_names (idNode, language, nodeName) VALUE (9, 'italian', 'Europa');
+INSERT INTO node_tree_names (idNode, language, nodeName) VALUE (10, 'italian', 'Sviluppatori');
+INSERT INTO node_tree_names (idNode, language, nodeName) VALUE (11, 'italian', 'Nord America');
+INSERT INTO node_tree_names (idNode, language, nodeName) VALUE (12, 'italian', 'Controllo Qualità');
 
